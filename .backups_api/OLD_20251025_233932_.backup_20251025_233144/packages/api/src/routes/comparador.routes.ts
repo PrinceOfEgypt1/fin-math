09f9f5b71490938e27f0d32ee78732b734a82dc7
Moises <mpmoses@gmail.com>
2025-10-25 23:48:34 -0300

    chore: ignore backups/debug e manter apenas fontes

--- .backups_api/OLD_20251025_233932_.backup_20251025_233144/packages/api/src/routes/comparador.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { compararCenariosEndpoint } from "../controllers/comparador.controller";
-
-const router = Router();
-
-router.post("/comparar", compararCenariosEndpoint);
-
-export default router;
